53e3b5f3dc388f28aaf06f2c1b2388486c8bbf5b
yuyinws <lyc657508366@gmail.com>
2024-01-27 18:45:25 +0800

    init

---
+++ src/stores/counter.js
@@ -0,0 +1,9 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useCounterStore = defineStore('counter', () => {
+  const count = ref(0)
+
+
+  return { count }
+})
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useCounterStore } from '../stores/counter'
+import { storeToRefs } from 'pinia'
+import { watch } from 'vue'
+
+const { count } = storeToRefs(useCounterStore())
+
+watch(count, (val) => {
+  console.log('store中的count变化了', val)
+})
+</script>
+
+<template>
+  <main>
+    {{ count }}
+  </main>
+  <button @click="count++">+</button>
+</template>
